a99c71b0d5179df9b9803e985e8aa8f051a830fd
Theodore Schnepper <ayiga@msn.com>
2016-03-27 11:20:37 -0600

    modify prefix to be more helpful
    change logging output backtrace level to properly reflect the caller,
    instead of the caller's caller

--- logger.go
+++ logger.go
@@ -6,7 +6,7 @@ import (
 	"os"
 )
 
-var debugLogger = log.New(os.Stdout, "apns", log.Ldate|log.Ltime|log.Lmicroseconds|log.Lshortfile)
+var debugLogger = log.New(os.Stdout, "apns: ", log.Ldate|log.Ltime|log.Lmicroseconds|log.Lshortfile)
 
 // DebugAPN is a flag that determins whether or not logging should be output, or
 // not
@@ -14,6 +14,6 @@ var DebugAPN = false
 
 func vlogf(format string, args ...interface{}) {
 	if DebugAPN {
-		debugLogger.Output(3, fmt.Sprintf(format, args...))
+		debugLogger.Output(2, fmt.Sprintf(format, args...))
 	}
 }
